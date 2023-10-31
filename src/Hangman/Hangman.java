7556bd7b59196011d82146c1b18be9ae34f273b5
Skorkin_Evgeniy <y.o.skorkin@student.khai.edu>
2023-11-01 01:23:28 +0200

    Hangman 3-rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] wordList = {"python", "java", "javascript", "kotlin"};
+        String wordToGuess = wordList[random.nextInt(wordList.length)];
+
         System.out.println("HANGMAN");
         System.out.print("Guess the word: > ");
-        String wordToGuess = "java"; // Задайте слово, яке гравець повинен вгадати
 
         String playerGuess = scanner.next();
 
