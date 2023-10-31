4fd658362e664ad4dfd1217dd3ed4b668484327b
Skorkin_Evgeniy <y.o.skorkin@student.khai.edu>
2023-11-01 01:24:46 +0200

    Hangman 4-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -9,8 +9,16 @@ public class Hangman {
         String[] wordList = {"python", "java", "javascript", "kotlin"};
         String wordToGuess = wordList[random.nextInt(wordList.length)];
 
+        // Відобразити перші 2 літери слова, решта літер будуть дефісами
+        StringBuilder displayWord = new StringBuilder();
+        displayWord.append(wordToGuess.charAt(0));
+        displayWord.append(wordToGuess.charAt(1));
+        for (int i = 2; i < wordToGuess.length(); i++) {
+            displayWord.append("-");
+        }
+
         System.out.println("HANGMAN");
-        System.out.print("Guess the word: > ");
+        System.out.print("Guess the word " + displayWord + ": > ");
 
         String playerGuess = scanner.next();
 
